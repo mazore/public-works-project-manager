91f07a71bc73f5780137813a0abd634188c683a0
Evan Mazor <evanmazor@gmail.com>
2023-05-06 00:00:58 -0700

    Safe guard

--- src/table_attributes/city.py
+++ src/table_attributes/city.py
@@ -15,6 +15,6 @@ for line in open('city_ids.csv'):
 
 class City(QLabel):
     def __init__(self, project):
-        super().__init__(CITY_ID_NAME_MAP[project['city_id']])
+        super().__init__(CITY_ID_NAME_MAP.get(project['city_id'], 'Error: Unknown City'))
 
         self.setStyleSheet('margin-left: 5px;')
